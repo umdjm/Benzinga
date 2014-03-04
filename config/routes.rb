44895687dcad51c871808711b70e2b04a789fdf2
DJ Martin <umdjm@hotmail.com>
2014-03-04 17:12:17 -0500

    Signup Redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ StockPicker::Application.routes.draw do
   resources :users
   resources :sessions
   resources :stock_picks
-  root to: 'users#new'
+  root to: 'stock_results#show'
 
   get '/results/:day', to: 'stock_results#show'
+  get '/results', to: 'stock_results#show'
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
