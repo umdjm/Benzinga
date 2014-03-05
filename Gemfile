9f3f72e8d97b5d8c2f3d5f1a3171fe912b7a57e1
DJ Martin <umdjm@hotmail.com>
2014-03-04 23:48:21 -0500

    Switch to benzinga stock api

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
+gem 'httparty'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -28,7 +29,6 @@ group :assets do
 end
 
 gem 'jquery-rails'
-gem 'stock_quote'
 
 # To use ActiveModel has_secure_password
 gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/stock_result.rb
+++ app/models/stock_result.rb
@@ -2,6 +2,10 @@ class StockResult < ActiveRecord::Base
   attr_accessible :result_date, :stock, :closing_price, :current_price
   has_many :stock_picks
 
+  def closing_time
+    Time.parse(self.result_date.to_s).change(:hour => 16)
+  end
+
   def market_open
     return true if self.closing_price.nil?
     return false
@@ -12,21 +16,15 @@ class StockResult < ActiveRecord::Base
   end
 
   def self.update_prices
-    StockResult.where(:closing_price => nil).each do |result|
-      quote = StockQuote::Stock.quote(result.stock)
-      if quote.response_code == 200
-        result.current_price = quote.ask
-        result.closing_price = quote.previous_close if Date.strptime(quote.last_trade_date, "%m/%d/%Y") >= result.result_date
-        result.save
-      end
-    end
-  end
+    url = CONFIG[:quote_url] + CONFIG[:stock_list].join(',')
+    quotes = HTTParty.get(url).parsed_response
 
-  def update_price
-    quote = StockQuote::Stock.quote(self.stock)
-    if quote.response_code == 200
-      self.current_price = quote.ask
-      self.closing_price = quote.previous_close if Date.strptime(quote.last_trade_date, "%m/%d/%Y") >= result.result_date
+    StockResult.where(:closing_price => nil).each do |result|
+      quote = quotes[result.stock]
+      quote_time = Time.at(quote["asktime"]/1000)
+      result.current_price = quote["ask"]
+      result.closing_price = quote["ask"] if quote_time >= result.closing_time
+      result.save
     end
   end
 
@@ -34,10 +32,13 @@ class StockResult < ActiveRecord::Base
     return unless StockResult.where(:closing_price => nil).last.nil?
     stock_date = Time.now
     weekdays = 1..5
+    url = CONFIG[:quote_url] + CONFIG[:stock_list].join(',')
+    quotes = HTTParty.get(url).parsed_response
+
     stock_date += 1.day until stock_date > Time.now && weekdays.member?(stock_date.wday)
 
-    CONFIG[:stock_list].each do |stock_symbol|
-      newRecord = StockResult.new(:result_date => stock_date, :stock => stock_symbol)
+    quotes.each do |quote|
+      newRecord = StockResult.new(:result_date => stock_date, :stock => quote["ipfSymbol"], :current_price => quote["ask"])
       newRecord.save
     end
   end
