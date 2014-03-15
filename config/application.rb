37bd3c2f997cbce3795f05767961556cf5e2a8c5
DJ Martin <umdjm@hotmail.com>
2014-03-15 12:59:58 -0400

    highlight user

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ CONFIG.symbolize_keys!
 
 module StockPicker
   class Application < Rails::Application
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
