9df2bc6d9777ce0f2eceaef91b085b6b54116f07
DJ Martin <umdjm@hotmail.com>
2014-03-28 08:14:12 -0400

    prevent duplicate picks

--- app/controllers/stock_picks_controller.rb
+++ app/controllers/stock_picks_controller.rb
@@ -6,7 +6,6 @@ class StockPicksController < ApplicationController
   def index
     @stock_picks = current_user.stock_picks.all
 
-
     @current_leaders = User.order("current_rank asc").limit(10)
     @current_leaders << current_user unless @current_leaders.include?(current_user)
 
@@ -55,9 +54,10 @@ class StockPicksController < ApplicationController
     @stock_pick.assigned_price = @stock_result.price
     @stock_pick.stock_result_id = @stock_result.id
     @stock_pick.prediction = params[:prediction]
+    duplicate_picks = StockPick.joins(:stock_result).where(:user_id => current_user.id, stock_results: {result_date: @stock_result.result_date})
 
     respond_to do |format|
-      if @stock_pick.save
+      if !duplicate_picks.empty? || @stock_pick.save
         format.html { redirect_to "/results/" + @stock_result.result_date.to_s, notice: "Pick Created"}
       else
         format.html { redirect_to "/results/" + @stock_result.result_date.to_s, notice: @stock_pick.errors.to_json}
